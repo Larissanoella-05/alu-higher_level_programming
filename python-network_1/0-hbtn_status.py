95452835d16c9bc598b243f643fcd3d91d61aed5
Larissanoella-05 <s.noella@alustudent.com>
2024-08-08 10:52:16 +0000

    initial

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
-"""Module that fetches the status from a given URL using urllib"""
-
 import urllib.request
 
-def fetch_status(url):
-    """Fetches and displays the status of a given URL."""
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
+def fetch_status():
+    url = 'https://alu-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", content.decode("utf-8"))
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
 
 if __name__ == "__main__":
-    fetch_status('https://intranet.hbtn.io/status')
-    fetch_status('http://0.0.0.0:5050/status')
+    fetch_status()
